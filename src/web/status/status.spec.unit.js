c2c52a50f5f32ff9fbb765b8904a849c8c6573e1
Rodrigo Gomes da Silva <rodrigo.smscom@gmail.com>
2018-04-30 20:17:47 -0300

    tests

--- src/web/status/status.spec.unit.js
+++ src/web/status/status.spec.unit.js
@@ -3,10 +3,43 @@
 const chai = require('chai');
 const chaiAsPromised = require('chai-as-promised');
 
+const StatusService = require('./status.service');
+
 before(() => {
   chai.use(chaiAsPromised);
   chai.should();
 });
 
 describe('StatusService', () => {
+
+  describe('#getUpTime', () => {
+
+    it('should return application up time', async () => {
+      const uptime = await StatusService.getUpTime();
+
+      uptime.should.be.a('number');
+    });
+
+  });
+
+  describe('#getHostInfo', () => {
+
+    it('should return full application host information', async () => {
+      const status = await StatusService.getHostInfo();
+
+      status.should.be.an('object');
+    }).timeout(10000); // Windows takes a lot to get host info
+
+  });
+
+  describe('#getStatus', () => {
+
+    it('should return application status', async () => {
+      const status = await StatusService.getStatus();
+
+      status.should.be.an('object');
+    }).timeout(10000); // Windows takes a lot to get host info
+
+  });
+
 });
